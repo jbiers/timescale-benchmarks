343ab6264fb75c07e535bc25e4b978f390717b4e
Julia Bier <juliabiersuriano@gmail.com>
2025-10-16 15:20:58 -0400

    fix error in csvreader.go and remove error capitalization

--- pkg/csvreader/csvreader.go
+++ pkg/csvreader/csvreader.go
@@ -18,7 +18,7 @@ func Stream(filePath string, chs []chan query.QueryData) error {
 	if filePath != "" {
 		file, err := os.Open(filePath)
 		if err != nil {
-			return fmt.Errorf("Error opening the query data file %s: %w", filePath, err)
+			return fmt.Errorf("error opening the query data file %s: %w", filePath, err)
 		}
 
 		// TODO: what are the real performance differences of using bufio.Reader vs a regular os.File?
@@ -35,12 +35,12 @@ func Stream(filePath string, chs []chan query.QueryData) error {
 
 	header, err := CSVreader.Read()
 	if err != nil {
-		return fmt.Errorf("Error parsing first line of query data file %s: %w", filePath, err)
+		return fmt.Errorf("error parsing first line of query data file %s: %w", filePath, err)
 	}
 
 	err = validateHeaderLine(header)
 	if err != nil {
-		return fmt.Errorf("Header validation failed for file %s: %w", filePath, err)
+		return fmt.Errorf("header validation failed for file %s: %w", filePath, err)
 	}
 
 	for {
@@ -49,12 +49,12 @@ func Stream(filePath string, chs []chan query.QueryData) error {
 			break
 		}
 		if err != nil {
-			return fmt.Errorf("Error reading CSV line from query data file %s: %w", filePath, err)
+			return fmt.Errorf("error reading CSV line from query data file %s: %w", filePath, err)
 		}
 
 		queryData, err := parseQueryData(record)
 		if err != nil {
-			return fmt.Errorf("Error parsing query data: %w", err)
+			return fmt.Errorf("error parsing query data: %w", err)
 		}
 
 		idx := queryData.GetHash(len(chs))
@@ -70,19 +70,19 @@ func Stream(filePath string, chs []chan query.QueryData) error {
 func validateHeaderLine(header []string) error {
 
 	if len(header) != 3 {
-		return fmt.Errorf("Expected three entries in header line")
+		return fmt.Errorf("expected three entries in header line")
 	}
 
 	if header[0] != "hostname" {
-		return fmt.Errorf("Expected first entry in header line to match 'hostname'")
+		return fmt.Errorf("expected first entry in header line to match 'hostname'")
 	}
 
 	if header[1] != "start_time" {
-		return fmt.Errorf("Expected first entry in header line to match 'start_time'")
+		return fmt.Errorf("expected first entry in header line to match 'start_time'")
 	}
 
 	if header[2] != "end_time" {
-		return fmt.Errorf("Expected first entry in header line to match 'end_time'")
+		return fmt.Errorf("expected first entry in header line to match 'end_time'")
 	}
 
 	return nil
@@ -90,18 +90,18 @@ func validateHeaderLine(header []string) error {
 
 func parseQueryData(record []string) (query.QueryData, error) {
 	if len(record) != 3 {
-		return query.QueryData{}, fmt.Errorf("Expected 3 entries in query data record, found %d", len(record))
+		return query.QueryData{}, fmt.Errorf("expected 3 entries in query data record, found %d", len(record))
 	}
 
 	timeStringFormat := "2006-01-02 15:04:05"
 	startTime, err := time.Parse(timeStringFormat, record[1])
 	if err != nil {
-		return query.QueryData{}, fmt.Errorf("Failed to parse 'start_time' as time.Time format: %w", err)
+		return query.QueryData{}, fmt.Errorf("failed to parse 'start_time' as time.Time format: %w", err)
 	}
 
-	endTime, err := time.Parse(timeStringFormat, record[1])
+	endTime, err := time.Parse(timeStringFormat, record[2])
 	if err != nil {
-		return query.QueryData{}, fmt.Errorf("Failed to parse 'end_time' as time.Time format: %w", err)
+		return query.QueryData{}, fmt.Errorf("failed to parse 'end_time' as time.Time format: %w", err)
 	}
 
 	return query.QueryData{
